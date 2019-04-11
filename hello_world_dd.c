fd08584bfb6cae22a42d0b4842ff3e2beb1b939e
k-kishan <49265808+k-kishan@users.noreply.github.com>
2019-04-11 12:28:52 +0530

    Add files via upload

---
+++ hello_world_dd.c
@@ -0,0 +1,25 @@
+#nclude<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/fs.h>
+
+static int __init hello_world_init(void)
+{
+	printk(KERN_INFO "Welcome to einfochips\n");
+        printk(KERN_INFO "This is the Simple Module\n");
+        printk(KERN_INFO "Kernel Module Inserted Successfully...\n");
+	return 0;
+}
+
+void __exit hello_world_exit(void)
+{
+	printk(KERN_INFO "Kernel Module Removed Successfully...\n");
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("KISHAN KAVA");
+MODULE_DESCRIPTION("A simple hello world driver");
+MODULE_VERSION("2:1.0");
---
+++ makefile
@@ -0,0 +1,2 @@
+obj-$(CONFIG_HELLO_WROLD_DD) := helloworlddd.o
+helloworlddd-objs := hello_world_dd.o
